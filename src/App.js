5fddc4a43d34b963ede9a17e9f2d1ab09752328b
Martin Banks <anotherbanksy@gmail.com>
2019-02-15 17:15:36 +1100

    adds basic layout of editor

--- src/App.js
+++ src/App.js
@@ -1,28 +1,128 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      before: false,
+      after: false,
+      start: 50,
+      embed: '',
+      showEmbed: true,
+    }
+    this.embedCode = this.embedCode.bind(this)
+    this.handleUpdateBefore = this.handleUpdateBefore.bind(this)
+    this.handleUpdateAfter = this.handleUpdateAfter.bind(this)
+    this.toggleEmbedCode = this.toggleEmbedCode.bind(this)
+  }
+
+  handleUpdateBefore (e) {
+    this.setState({ before: e.target.value})
+    this.embedCode()
+  }
+  
+  handleUpdateAfter (e) {
+    this.setState({ after: e.target.value})
+    this.embedCode()
+  }
+
+  toggleEmbedCode () {
+    this.setState({ showEmbed: !this.state.showEmbed })
+  }
+
+  embedCode () {
+    const template = `
+      <div>
+        <link url="https://where-the-styles-live" rel="stylesheet" />
+          <div id="slider-container">
+            <div id="slider" class="beer-slider" data-beer-label="before">
+              <img src="${this.state.before} alt="">
+              <div class="beer-reveal" data-beer-label="after">
+                <img src="${this.state.after}" alt="">
+              </div>
+            </div>
+          </div>
+        <script src="https://where-the-scripts-are" type="text/javascript"></script>
+        <script type="text/javascript">
+          new BeerSlider(
+            document.querySelector('#slider-container'),
+            { start: ${this.state.start}})
+        </script>
+      </div>
+    `
+    this.setState({ embed: template })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Before / after slider maker</h1>
+
+        <form action="">
+        
+          <label htmlFor="beforeImage">URL of before image</label>
+          <input
+            id="beforeImage"
+            name="beforeImage"
+            type="url"
+            onChange={ this.handleUpdateBefore }
+          />
+
+          <label htmlFor="afterImage">URL of after image</label>
+          <input
+            id="afterImage"
+            name="beforeImage"
+            type="url"
+            onChange={ this.handleUpdateAfter }
+          />
+
+          <label htmlFor="start">Start position</label>
+          <input type="range" value={ this.state.start }/>
+
+
+        </form>
+        <button onClick={ this.toggleEmbedCode }>
+          { `${this.state.showEmbed ? 'Hide' : 'Show'} embed code` }
+        </button>
+        {
+          this.state.showEmbed
+            ? <pre className="embedCode">{ this.state.embed }</pre>
+            : ''
+        }
+
+        <div id="preview"></div>
+
+
+        <style jsx>{`
+          * {
+            text-align: left;
+          }
+
+          form {
+            text-align: left;
+            margin: 20px auto;
+            width: 80%;
+            max-width: 1000px;
+          }
+
+          input {
+            display: block;
+            margin-bottom: 20px;
+          }
+          label {
+            display: block;
+          }
+
+          pre {
+            background: #333;
+            color: #bada55;
+            padding: 20px;
+          }
+          
+        `}</style>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
